c1b66c3a61ab47de5c96256c9f80f7e4286da1c2
basita512 <mail2basita@gmail.com>
2025-02-20 11:34:53 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+
+function App() {
+  
+    return (
+        <div className="">
+            <Routes>
+                <Route path='/home' />
+                <Route path='/advaiya'/>
+            </Routes>
+        </div>
+            
+    )
+}
+
+export default App
