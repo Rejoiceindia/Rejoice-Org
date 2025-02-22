91fe13b1203416b4c85659bfc135383393ba319d
basita512 <mail2basita@gmail.com>
2025-02-23 00:37:37 +0530

    hover fix

--- src/components/Home-page/Navbar.tsx
+++ src/components/Home-page/Navbar.tsx
@@ -1,7 +1,6 @@
 import { FaGithub } from "react-icons/fa"
 import { HiSun, HiMoon } from "react-icons/hi"
-// import useTheme from "../../Context/useTheme"
- import { useTheme } from "../../Context/ThemeContext"      
+import { useTheme } from "../../Context/ThemeContext"      
 
 const Navbar = () => {
     const { theme, toggleTheme } = useTheme()
@@ -27,7 +26,7 @@ const Navbar = () => {
                 <a href="https://github.com/Rejoiceindia" 
                    target="_blank"
                    rel="noopener noreferrer"
-                   className=" p-2 text-gray-700 dark:text-white hover:text-gray-900 dark:hover:text-gray-300 transition-colors">
+                   className="text-black p-2 rounded-full dark:text-white hover:bg-gray-100 dark:hover:bg-[#0d0d0d] transition-colors">
                         <FaGithub className="w-6 h-6" />
                 </a>
             </div>
