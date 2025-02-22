3d2706367ebe573722992a37e3c5a9568d9f8bef
basita512 <mail2basita@gmail.com>
2025-02-22 21:15:30 +0530

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
-
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Home from './pages/Home'
 
 function App() {
-  
     return (
-        <div className="">
+        <div className="bg-[#F5F5F5] dark:bg-[#0d0d0d] min-h-screen transition-colors duration-300">
             <Routes>
-                <Route path='/home' />
+                <Route path='/' element={<Home/>} />
                 <Route path='/advaiya'/>
             </Routes>
         </div>
-            
     )
 }
 
