24011d52278ea70cd0ed06d8a4737f9f9b3d1abb
basita512 <mail2basita@gmail.com>
2025-02-24 23:44:48 +0530

    added component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,8 @@ import Navbar from "../components/Home-page/Navbar"
 import Tagline from "../components/Home-page/Tagline"
 import JoinCommunity from "../components/Home-page/JoinCommunity"
 import Footer from "../components/Home-page/Footer"
+import ProjectCard from "../components/Home-page/ProjectCard"
+import { ProjectDetails } from "../Data/ProjectCard"
 
 const Home = () => {
   return (
@@ -12,6 +14,16 @@ const Home = () => {
 
         <div className="tagline">
             <Tagline/>
+        </div>
+
+        <h1 className="text-3xl text-center tracking-wider font-bold mx-40 dark:text-white dark:duration-300">
+            OUR PROJECTS
+        </h1>
+
+        <div className="project-cards flex flex-wrap justify-center mt-12 gap-8">
+            {ProjectDetails.map((project)=> (
+                <ProjectCard key={project.id} {...project}/>
+            ))}
         </div>
 
         <JoinCommunity />
