93aca8cdf5531d5b9fb3a590fdd29a8a41d45611
basita512 <mail2basita@gmail.com>
2025-02-25 23:41:53 +0530

    component added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,19 @@
-import Navbar from "../components/Home-page/Navbar"
-import Tagline from "../components/Home-page/Tagline"
+// import Navbar from "../components/Home-page/Navbar"
+// import Tagline from "../components/Home-page/Tagline"
 import JoinCommunity from "../components/Home-page/JoinCommunity"
 import Footer from "../components/Home-page/Footer"
 import ProjectCard from "../components/Home-page/ProjectCard"
 import { ProjectDetails } from "../Data/ProjectCard"
+import MainSection from "../components/Home-page/MainSection"
+
 
 const Home = () => {
   return (
     <div className="w-full min-h-screen">
-        <div className="navbar sm:px-6 lg:px-8 py-4 sticky top-0 z-50">
-            <Navbar/>
-        </div>
-
-        <div className="tagline">
-            <Tagline/>
-        </div>
-
-        <h1 className="text-3xl text-center tracking-wider font-bold mx-40 dark:text-white dark:duration-300">
+        
+        <MainSection/>
+        
+        <h1 className="text-3xl text-center tracking-wider font-bold dark:text-white dark:duration-300">
             OUR PROJECTS
         </h1>
 
