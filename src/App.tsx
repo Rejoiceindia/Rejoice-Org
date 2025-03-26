e8a585cbd86eb285dd707bfc7dfe9554b3f5ec28
basita512 <mail2basita@gmail.com>
2025-03-27 01:14:55 +0530

    removed theme logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
-import ProjectPage from './pages/ProjectPage';
-// import Home from './pages/Home';
-import NewHome from './pages/NewHome';
+import ProjectPage from './pages/ProjectPage'
+import NewHome from './pages/Home';
 
 function App() {
     return (
         <div className="bg-[#F5F5F5] dark:bg-black min-h-screen transition-colors duration-300">
             <Routes>
-                {/* <Route path='/' element={<Home/>}/> */}
                 <Route path='/' element={<NewHome/>}/>
                 <Route path='/advaiya' element={<ProjectPage />} />
             </Routes>
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
-import { ThemeProvider } from './Context/ThemeContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
-        <ThemeProvider>
             <App />
-        </ThemeProvider>
     </BrowserRouter>
 
 )
